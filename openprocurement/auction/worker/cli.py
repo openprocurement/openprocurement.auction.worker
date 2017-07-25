3964ad15a4d28a71d0ab1cc424b6e91aa3a7f419
Tuoris <tuoris.dev@gmail.com>
2017-07-25 16:24:36 +0300

    Integrate Prozorro Sale auction into esco

--- openprocurement/auction/worker/cli.py
+++ openprocurement/auction/worker/cli.py
@@ -19,10 +19,10 @@ def main():
     parser.add_argument('auction_doc_id', type=str, help='auction_doc_id')
     parser.add_argument('auction_worker_config', type=str,
                         help='Auction Worker Configuration File')
-    parser.add_argument('--auction_info', type=str, help='Auction File')
+    parser.add_argument('--auction_info', type=str, help='Get auction data from file')
     parser.add_argument('--auction_info_from_db', type=str, help='Get auction data from local database')
-    parser.add_argument('--with_api_version', type=str, help='Tender Api Version')
-    parser.add_argument('--lot', type=str, help='Specify lot in tender', default=None)
+    parser.add_argument('--with_api_version', type=str, help='Resource Api Version')
+    parser.add_argument('--lot', type=str, help='Specify lot in resource', default=None)
     parser.add_argument('--planning_procerude', type=str, help='Override planning procerude',
                         default=None, choices=[None, C.PLANNING_FULL, C.PLANNING_PARTIAL_DB, C.PLANNING_PARTIAL_CRON])
 
@@ -31,13 +31,14 @@ def main():
     if os.path.isfile(args.auction_worker_config):
         worker_defaults = yaml.load(open(args.auction_worker_config))
         if args.with_api_version:
-            worker_defaults['TENDERS_API_VERSION'] = args.with_api_version
+            worker_defaults['resource_api_version'] = args.with_api_version
         if args.cmd != 'cleanup':
             worker_defaults['handlers']['journal']['TENDER_ID'] = args.auction_doc_id
             if args.lot:
                 worker_defaults['handlers']['journal']['TENDER_LOT_ID'] = args.lot
-        for key in ('TENDERS_API_VERSION', 'TENDERS_API_URL',):
-            worker_defaults['handlers']['journal'][key] = worker_defaults[key]
+        
+        worker_defaults['handlers']['journal']['TENDERS_API_VERSION'] = worker_defaults['resource_api_version']
+        worker_defaults['handlers']['journal']['TENDERS_API_URL'] =  worker_defaults['resource_api_server']
 
         logging.config.dictConfig(worker_defaults)
     else:
